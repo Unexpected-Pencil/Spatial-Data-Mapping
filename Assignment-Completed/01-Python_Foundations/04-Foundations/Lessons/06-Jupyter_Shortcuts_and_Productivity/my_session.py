e460ab71879b3fa08e8df8c5341cee4f47db50fb
Unexpected-Pencil <Jakelawler2005@gmail.com>
2026-05-06 23:52:54 +0000

    cleanup

---
+++ Assignment-Completed/01-Python_Foundations/04-Foundations/Lessons/06-Jupyter_Shortcuts_and_Productivity/my_session.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('save', 'my_session.py 1-10')
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('save', 'my_session.py 1-10')
